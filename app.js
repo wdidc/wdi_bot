c1aab5c5d7e26e3a099b633862facb8993d7bdd8
Adrian Maseda <amaseda@gmail.com>
2015-08-05 15:41:20 -0400

    bot can post hardcoded message to slack

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 var env = require( "./env" );
+var request = require( "request" );
 
-console.log( env.token );
+request( "https://slack.com/api/groups.list?token=" + env.token, function( err, response, body ){
+  console.log( body );
+});
+
+var postSomething = function( text ){
+  var url = "https://slack.com/api/chat.postMessage?token=" + env.token + "&channel=" + env.group_id + "&text=" + text + "&username=AnonBot"
+  request( url, function( err, response, body ){
+    console.log( body );
+  });
+}
+
+postSomething( "beep boop" );

edc773668ad5e0a176053dc3a813bb9dcf95f07f
Adrian Maseda <amaseda@gmail.com>
2015-08-05 16:07:43 -0400

    AnonBot can listen for events

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 var env = require( "./env" );
 var request = require( "request" );
+var WebSocket = require ( "ws" );
 
-request( "https://slack.com/api/groups.list?token=" + env.token, function( err, response, body ){
-  console.log( body );
+// request( "https://slack.com/api/groups.list?token=" + env.token, function( err, response, body ){
+//   console.log( body );
+// });
+
+// var postSomething = function( text ){
+//   var url = "https://slack.com/api/chat.postMessage?token=" + env.token + "&channel=" + env.group_id + "&text=" + text + "&username=AnonBot"
+//   request( url, function( err, response, body ){
+//     console.log( body );
+//   });
+// }
+
+// if {message: ...} starts with D, AnonBot has received a direct message
+// direct message is stored in {text: ...}
+// if {text} contains <@U08LS74MR>, then we know it is a mention to AnonBot
+// if {channel} is G08LT3JQ4, it is a direct message to AnonBot
+
+
+
+request( "https://slack.com/api/rtm.start?token=" + env.token, function( err, response, body ){
+  var ws = new WebSocket( JSON.parse( body ).url );
+  ws.on( "message", function( message ){
+    console.log( message );
+  })
 });
-
-var postSomething = function( text ){
-  var url = "https://slack.com/api/chat.postMessage?token=" + env.token + "&channel=" + env.group_id + "&text=" + text + "&username=AnonBot"
-  request( url, function( err, response, body ){
-    console.log( body );
-  });
-}
-
-postSomething( "beep boop" );

1e28fa90b6dc01d6b9660d7097f76addde3cf5b3
Adrian Maseda <amaseda@gmail.com>
2015-08-05 16:49:33 -0400

    can distinguish message types

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 var env = require( "./env" );
 var request = require( "request" );
 var WebSocket = require ( "ws" );
-
-// request( "https://slack.com/api/groups.list?token=" + env.token, function( err, response, body ){
-//   console.log( body );
-// });
-
-// var postSomething = function( text ){
-//   var url = "https://slack.com/api/chat.postMessage?token=" + env.token + "&channel=" + env.group_id + "&text=" + text + "&username=AnonBot"
-//   request( url, function( err, response, body ){
-//     console.log( body );
-//   });
-// }
-
-// if {message: ...} starts with D, AnonBot has received a direct message
-// direct message is stored in {text: ...}
-// if {text} contains <@U08LS74MR>, then we know it is a mention to AnonBot
-// if {channel} is present, 
-
-
+var message = require( "./message" );
 
 request( "https://slack.com/api/rtm.start?token=" + env.token, function( err, response, body ){
   var ws = new WebSocket( JSON.parse( body ).url );
-  ws.on( "message", function( message ){
-    console.log( message );
+  ws.on( "message", function( msgObj ){
+    var msg = message( msgObj );
+    if( msg.isMention() ){
+      console.log( "User was mentioned" );
+    }
+    else if( msg.isDirectMessage() ){
+      console.log( "User was DM'd" );
+    }
   })
 });
--- message.js
+++ message.js
@@ -1,10 +1,22 @@
 module.exports = function( message ){
+  message = JSON.parse( message );
   return {
     isMention: function(){
-      return message.text.match( "<@U08LS74MR>" );
+      return message.text && message.text.match( "<@U08LS74MR>" );
     },
     isDirectMessage: function(){
-      return message.channel[0] == "D";
+      return message.channel && message.channel[0] == "D";
     }
   }
 }
+
+// request( "https://slack.com/api/groups.list?token=" + env.token, function( err, response, body ){
+//   console.log( body );
+// });
+
+// var postSomething = function( text ){
+//   var url = "https://slack.com/api/chat.postMessage?token=" + env.token + "&channel=" + env.group_id + "&text=" + text + "&username=AnonBot"
+//   request( url, function( err, response, body ){
+//     console.log( body );
+//   });
+// }

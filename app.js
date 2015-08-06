61ea2b29ef16ed7134b6083b81e0c31a7b0c82da
Jesse Shawl <jesse@jshawl.com>
2015-08-05 20:07:36 -0400

    rearrange files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var env = require( "./env" );
 var request = require( "request" );
 var WebSocket = require ( "ws" );
-var message = require( "./message" );
+var message = require( "./lib/message" );
 
 request( "https://slack.com/api/rtm.start?token=" + env.token, function( err, response, body ){
   var ws = new WebSocket( JSON.parse( body ).url );

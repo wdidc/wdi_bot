4b3d5ee795e19a22184207d2ab11f8a0e1d73e7b
Jesse Shawl <jesse@jshawl.com>
2015-08-05 20:24:44 -0400

    add more tests

--- test/slack.js
+++ test/slack.js
@@ -1,8 +1,31 @@
 var assert = require("assert")
 var slack = require("../lib/slack")
+var env = require("../env")
+var client = slack(env.token)
 
 describe("slack", function(){
   it("is a function", function(){
     assert(typeof slack == "function")
+  })
+  it("has an access token", function(){
+    assert(client.token)
+  })
+  it("checks in with the slack api", function(done){
+    client.api.test(function(response){
+      assert(response.ok) 
+      done()
+    })
+  })
+  it("tests authentication", function(done){
+    client.auth.test(function(response){
+      assert(response.ok) 
+      done()
+    })
+  })
+  it("lists channels", function(done){
+    client.channels.list(function(response){
+      assert(response.ok) 
+      done()
+    })
   })
 })

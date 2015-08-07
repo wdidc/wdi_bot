ecf1c4dd80f20e4f806cb37448521b9f6a8999dc
RobertAKARobin <robertgfthomas@gmail.com>
2015-08-07 12:06:27 -0400

    Remodularized a bit

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 var env = require( "./env" );
+var h = require("./lib/h")()
 var request = require( "request" );
+
 var WebSocket = require ( "ws" );
-var message = require("./lib/message")
+var Anonbot = require("./lib/anonbot")();
 
 request( "https://slack.com/api/rtm.start?token=" + env.token, function( err, response, body ){
   var ws = new WebSocket( JSON.parse( body ).url );
 
-  ws.on( "message", message);
+  ws.on( "message", function( message ){
+    message = JSON.parse(message);
+
+    if(message.text && message.text.match( env.bot_id )){
+      message.type = "mention";
+    }else if(message.text && message.channel && message.channel[0] == "D"){
+      message.type = "dm";
+    }else{
+      return
+    }
+    message.text = message.text.replace("<@" + env.bot_id + ">: ", "");
+
+    switch(message.type){
+      case "mention":
+        console.log("***Received mention from " + message.user);
+        if(message.channel == env.private_group_id && message.user != env.bot_id){
+          Anonbot.instructor_message( message )
+        }
+        break;
+      case "dm":
+        console.log("***Received DM from " + message.user);
+        Anonbot.student_message( message )
+        break;
+    }
+  });
 })

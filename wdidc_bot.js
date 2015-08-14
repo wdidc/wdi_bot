9c70e54a1e32b477dd97d23a79d00a0689397453
RobertAKARobin <robertgfthomas@gmail.com>
2015-08-14 11:10:24 -0400

    Committing before reverting back to Anonbot

--- wdidc_bot.js
+++ wdidc_bot.js
@@ -1,10 +1,15 @@
 var request   = require( "request" );
-var WebSocket = require ( "ws" );
+var WebSocket = require( "ws" );
 var env       = require( "./env" );
+var fs        = require("fs");
 
 var h       = require("./lib/helper")();
 var Message = require("./lib/message");
 var SlackAPI= require("./lib/slack")();
+var response = {
+  commands: fs.readFile(__dirname + "/assets/commands.txt"),
+  commands_instructor: fs.readFile(__dirname + "/assets/commands_instructor.txt")
+}
 
 SlackAPI.refresh_groups();
 
@@ -13,37 +18,50 @@ request("https://slack.com/api/rtm.start?token=" + env.token, function(err,respo
 
   ws.on( "message", function( message ){
     var m = Message( JSON.parse(message) );
-    if(!m){
-      return;
-    }else{
-      console.log(m);
-    }
+    if(!m) return;
+    else console.log("***New message received: " + JSON.stringify(m));
 
     if(m.group == "dm"){
-      if(m.sender == "instructor") m.reply("Hey, Instructor! Please @mention your message to me in `" + global.bot.private_group_name + "`. DMs are intended for students.");
-      if(m.sender == "student"){
-        if(m.intent == "repost"){
-          m.repost({from: m.sender, to: env.public_group_id})
-          SlackAPI.get_username(m.user, function(username){
-            m.repost({from: username, to: env.private_group_id})
-          });
+      if(m.intent == "command"){
+        switch(m.command.name.toLowerCase()){
+          case "help":
+            m.reply(response.commands);
+            break;
+          case "anonymous":
+            m.repost({from: m.sender, to: env.public_group_id})
+            SlackAPI.get_username(m.user, function(username){
+              m.repost({from: username, to: env.private_group_id})
+            });
+            break;
+          case "edit":
+            if(m.sender != "instructor") return;
+            if(!m.edit(m.command.args[0])) return;
+            SlackAPI.get_username(m.user, function(username){
+              m.repost({
+                from: username,
+                to: env.private_group_id,
+                message: "Edited " + h.get_time(m.command.args[0]) + ": " + m.text
+              })
+            });
+            break;
+          case "delete":
+            if(m.sender != "instructor") return;
+            if(!m.delete(m.command.args[0])) return;
+            SlackAPI.get_username(m.user, function(username){
+              m.repost({
+                from: username,
+                to: env.private_group_id,
+                message: "Deleted " + h.get_time(m.command.args[0])
+              })
+            });
+            break;
+          default:
+            m.reply("Command not recognized.")
+            break;
         }
+      }else{
+        m.reply("Hi there! Give me commands by direct messaging me with the command followed by `:`. For example:\n```\nHelp:\n```");
       }
-    }else
-    if(m.group == "public"){
-
-    }else
-    if(m.group == "private"){
-      if(m.is_mention){
-        if(m.intent == "repost") m.repost({from: m.sender, to: env.public_group_id});
-        if(m.intent == "command"){
-          if(m.command == "edit") m.edit(m.time);
-          if(m.command == "delete") m.delete(m.time);
-          if(m.command == "refresh") SlackAPI.refresh_groups();
-        }
-      }
-      if(m.intent == "join") SlackAPI.refresh_groups();
-      if(m.intent == "leave") SlackAPI.refresh_groups();
     }
 
   });

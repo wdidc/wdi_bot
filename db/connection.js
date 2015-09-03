37b31ea34607715ab57f457be2cbca240be492c9
Jesse Shawl <jesse@jshawl.com>
2015-09-03 11:29:09 -0400

    fixed connection to use current directory

--- db/connection.js
+++ db/connection.js
@@ -2,8 +2,8 @@ var Sequelize = require("sequelize");
 var SQLite    = require("sqlite3");
 var db_connection = new Sequelize("wdi_bot", null, null, {
   dialect: "sqlite",
-  connection: new SQLite.Database("db/db.sqlite"),
-  storage: "db/db.sqlite"
+  connection: new SQLite.Database(__dirname + "/db.sqlite"),
+  storage: __dirname + "/db.sqlite"
 });
 
 var Reminder = db_connection.define("reminder", {
